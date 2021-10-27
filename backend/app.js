41afa66c2bba2e378e59bc5fe2f42591c5130fb3
Alan <alan.vega@outlook.es>
2021-10-26 20:59:24 -0500

    9

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.use('/api', roomRoutes)
 
 
-app.get('/', (req,res) => {
+app.get('/', (res) => {
     res.status(200).send(
         "<H1>Pagina inicio API</H1>"
     )
